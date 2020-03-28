4eca8a13d02c982d778cbae04e80f6ade03cdc3d
rubaiyadoula <rubaiyadoula@gmail.com>
2020-03-29 01:35:03 +0600

    assert dollar object instead of amount

--- src/test/java/me/rubaiya/MoneyTest.java
+++ src/test/java/me/rubaiya/MoneyTest.java
@@ -16,7 +16,7 @@ public class MoneyTest {
     void multiplicationTest() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), product);
     }
 
     @Test
