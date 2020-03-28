855d16d06e3094787b62eb29bde8be70a24ed51c
rubaiyadoula <rubaiyadoula@gmail.com>
2020-03-29 01:50:15 +0600

    test different currency equality

--- src/test/java/me/rubaiya/MoneyTest.java
+++ src/test/java/me/rubaiya/MoneyTest.java
@@ -13,15 +13,16 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void multiplicationTest() {
+    void dollarMultiplicationTest() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(new Dollar(10), product);
     }
 
     @Test
-    void equalityTest() {
+    void dollarEqualityTest() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(6));
+        assertNotEquals(new Franc(5), new Dollar(5));
     }
 }
