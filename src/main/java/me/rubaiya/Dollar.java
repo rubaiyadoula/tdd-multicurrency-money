e41839082f6b466697b96ad47c6457a51071b762
rubaiyadoula <rubaiyadoula@gmail.com>
2020-04-10 21:54:15 +0600

    times method returns Money -> Dollar

--- src/main/java/me/rubaiya/Dollar.java
+++ src/main/java/me/rubaiya/Dollar.java
@@ -1,14 +1,12 @@
 package me.rubaiya;
 
-import java.util.Objects;
-
 public class Dollar extends Money {
 
     public Dollar(int amount) {
         this.amount = amount;
     }
 
-    Dollar times(int multipliers) {
+    Money times(int multipliers) {
         return new Dollar(amount * multipliers);
     }
 }
